0863698b467325fbfc24e9f3ed648b8f61b2ee26
Tanuka16 <das.tanuka@yahoo.com>
2019-05-17 12:02:03 -0400

    Done.

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,7 @@
 require_relative "./vehicle.rb"
+#user = admins, instructors or students
+class Car < Vehicle
+  def go                #lets you make changes of the vehicle class
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
